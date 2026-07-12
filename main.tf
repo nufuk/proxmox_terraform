e0d3c0110f836cbd3b1335f04ae0d19e502016fa
nufuk <info@nufuk.com>
2026-07-12 14:38:40 +0200

    cleanup

--- main.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "http" {
-    address        = "https://k8.servehttp.com:30443/api/v1/repos/terraform-states/gipromez/state"
-    lock_address   = "https://k8.servehttp.com:30443/api/v1/repos/terraform-states/gipromez/lock"
-    unlock_address = "https://k8.servehttp.com:30443/api/v1/repos/terraform-states/gipromez/unlock"
-  }
-}
